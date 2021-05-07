278bd4b8bb7c5a3afc2aaba84428fc38ecb058ce
Anthony Brown <ac524.brown@gmail.com>
2021-05-07 12:29:38 -0700

    Created strategy for handling location specific back links (not fully tested)

--- app/OddDog/ClientReviewForm/Views/OddDogLinkBack.php
+++ app/OddDog/ClientReviewForm/Views/OddDogLinkBack.php
@@ -3,6 +3,7 @@
 namespace OdReviewForm\OddDog\ClientReviewForm\Views;
 
 use OdReviewForm\OddDog\ClientReviewForm\Account\AccountInfo;
+use OdReviewForm\OddDog\ClientReviewForm\Locations\Locations;
 
 class OddDogLinkBack extends HtmlOutput
 {
@@ -12,18 +13,33 @@ class OddDogLinkBack extends HtmlOutput
     /** @var string */
     private $text;
 
-    public function __construct()
+    public function __construct( ?string $locationKey )
     {
+        $options = $locationKey && Locations::instance()->containsKey( $locationKey ) ? $this->getLocationOptions( $locationKey ) : [ 'url' => "", 'text' => "" ];
+
         $settings = AccountInfo::instance()->settings;
 
-        $this->url = $settings->backLinkUrl ?: "https://odd.dog/";
+        if( !$options['url'] && $settings->backLinkUrl ) $options['url'] = $settings->backLinkUrl;
+        if( !$options['text'] && $settings->backLinkText ) $options['text'] = $settings->backLinkText;
 
-        $this->text = $settings->backLinkText ?: "Reviews Powered by Odd Dog Media";
+        $this->url = $options['url'] ?: "https://odd.dog/";
+
+        $this->text = $options['text'] ?: "Reviews Powered by Odd Dog Media";
     }
 
     public function getHtml(): string
     {
         return sprintf('<a href="%s">%s</a>', $this->url, $this->text);
+    }
+
+    private function getLocationOptions( string $locationKey ) : array
+    {
+        $location = Locations::instance()->get($locationKey);
+
+        return [
+            'url' => $location->backLinkUrl,
+            'text' => $location->backLinkText
+        ];
     }
 
 }
